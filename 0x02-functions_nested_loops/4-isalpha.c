110a3d97bb0893ccd36679be8dd4f54e8899e0ca
zayed asem <zayedzezo122@gmail.com>
2023-10-16 07:54:27 -0700

    Write a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - checks for a lowercase character
+ *@c: letter being tested
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
