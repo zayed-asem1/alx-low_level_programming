2ae6a9989b762073e5a67ce24c71e556c6bb53eb
zayed asem <zayedzezo122@gmail.com>
2023-10-18 08:12:50 -0700

    modify file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,9 +4,9 @@
  * times_table - prints the 9 times table, starting with 0
  */
 
-void time_table(void)
+void times_table(void)
 {
-int i, j, k;
+t i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
