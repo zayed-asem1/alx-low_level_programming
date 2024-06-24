2258197367e40849ef8dee4509d2577a98e64153
zayed <zayedzezo122@gmail.com>
2024-06-24 12:28:50 -0700

    modify file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char letter;
+char letter;
 
-  for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
   {
-    putchar(letter);
+putchar(letter);
   }
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
