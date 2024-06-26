489c74adce1a1b49d8a81c84b8a798dc39bf4818
zayed <zayedzezo122@gmail.com>
2024-06-26 03:45:57 -0700

    modify file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * This program prints all single-digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i;
+
 for (i = 0; i < 10; i++)
 {
-putchar(i + '0'); // Convert integer to its corresponding character
+putchar(i + '0'); /* Convert integer to its corresponding character */
 }
-putchar('\n'); // Print a newline at the end
-return 0;
+putchar('\n'); /* Print a newline at the end */
+return (0);
 }
