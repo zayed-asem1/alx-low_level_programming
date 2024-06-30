610fafd692cf2a973aea1ff1b4d6d98c1eb14e16
zayed <zayedzezo122@gmail.com>
2024-06-30 05:58:28 -0700

    modify header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,49 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 /**
-*File:main.h
-*
-*Desc:header file containing functions declaration
-*/
+ * File: main.h
+ * Author: Pericles Adjovi
+ * Description: Header file containing function declarations.
+ */
 
+/* Function to print the alphabet in lowercase */
 void print_alphabet(void);
+
+/* Function to print every minute of the day of Jack Bauer */
 void jack_bauer(void);
+
+/* Function to print the alphabet 10 times */
 void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
-int _putchar(char);
+
+/* Function to print the 9 times table, starting with 0 */
+void times_table(void);
+
+/* Function to print all natural numbers from n to 98 */
+void print_to_98(int n);
+
+/* Function to print the n times table, starting with 0 */
+void print_times_table(int n);
+
+/* Function to print a character */
+int _putchar(char c);
+
+/* Function to check for lowercase character */
 int _islower(int c);
+
+/* Function to check for alphabetic character */
 int _isalpha(int c);
+
+/* Function to print the sign of a number */
 int print_sign(int n);
-int _abs(int);
+
+/* Function to compute the absolute value of an integer */
+int _abs(int n);
+
+/* Function to add two integers */
 int add(int n, int m);
+
+/* Function to print the last digit of a number */
 int print_last_digit(int n);
+
+#endif /* MAIN_H */
