1b457cd4ed8a2859d4f9375801dc280c9cffd16b
zayed <zayedzezo122@gmail.com>
2024-06-30 05:56:19 -0700

    modify header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c); /* Prototype for _islower function */
 
-#endif /* MAIN_H */
+/**
+*File:main.h
+*
+*Desc:header file containing functions declaration
+*/
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
