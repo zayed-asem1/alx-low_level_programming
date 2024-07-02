be61002a10b3d672038fe5caf462796a4ee522f4
zayed <zayedzezo122@gmail.com>
2024-07-02 04:40:10 -0700

    modfy header file

--- 0x02-functions_nested_loops/my_isalpha.h
+++ 0x02-functions_nested_loops/my_isalpha.h
@@ -1,7 +1,11 @@
-// my_isalpha.h
 #ifndef MY_ISALPHA_H
 #define MY_ISALPHA_H
 
+/* 
+ * my_isalpha.h
+ * Description: Header file for _isalpha function
+ */
+
 int _isalpha(int c);
 
-#endif // MY_ISALPHA_H
+#endif /* MY_ISALPHA_H */
