a2bfdfa07c0cc93741e1f65ee26707da5b36f7a9
zayed <zayedzezo122@gmail.com>
2024-07-02 08:38:07 -0700

    create file to test function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+i = 0;/* change made here */
+positive_or_negative(i);
+return (0);
+}
