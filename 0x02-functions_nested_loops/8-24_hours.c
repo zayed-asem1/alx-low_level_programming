f1d9448793006b82e8a71e05326417bdb8356263
zayed <zayedzezo122@gmail.com>
2024-08-24 06:13:14 -0700

    modify file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ int hour, minute;
 for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
-        {
+{
 _putchar('0' + hour / 10);
 _putchar('0' + hour % 10);
 _putchar(':');
